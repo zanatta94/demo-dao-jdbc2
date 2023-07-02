e90bb8439962fb7927ab0a47434155eebc31be9a
zanatta94 <felipezanattabirck@gmail.com>
2023-07-02 16:08:56 -0300

    Department entite Class

---
+++ src/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+import model.entites.Department;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Department obj = new Department(1, "Books");
+		
+		System.out.println(obj);
+
+
+	}
+
+}
