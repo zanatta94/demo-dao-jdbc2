4760fdb92c7529331745fb8b3d323922dcab2472
zanatta94 <felipezanattabirck@gmail.com>
2023-07-04 16:03:25 -0300

    Seller entite class

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,14 +1,18 @@
 package main;
 
+import java.util.Date;
+
 import model.entites.Department;
+import model.entites.Seller;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Books");
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
 		
-		System.out.println(obj);
+		System.out.println(seller);
 
 
 	}
