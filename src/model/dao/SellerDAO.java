45b98379f0eaaea930f1289e3ee22e448e7d7da4
zanatta94 <felipezanattabirck@gmail.com>
2023-07-04 16:38:27 -0300

    DepartmentDAO e SellerDAO

---
+++ src/model/dao/SellerDAO.java
@@ -0,0 +1,14 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entites.Seller;
+
+public interface SellerDAO {
+	void insert(Seller obj);
+	void update(Seller obj);
+	void deleteById(Integer id);
+	Seller findById(Integer id);
+	List<Seller> findaAll();
+
+}
